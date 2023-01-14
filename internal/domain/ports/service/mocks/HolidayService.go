cba0eeb4fbf6ab65b629dfe09cd2aeeddc3bfe4a
ManuelLecaro <39098566+ManuelLecaro@users.noreply.github.com>
2023-01-13 21:18:48 -0500

    Update HolidayService.go

--- internal/domain/ports/service/mocks/HolidayService.go
+++ internal/domain/ports/service/mocks/HolidayService.go
@@ -15,15 +15,15 @@ type HolidayService struct {
 }
 
 // GetHolidays provides a mock function with given fields: ctx, holiday
-func (_m *HolidayService) GetHolidays(ctx context.Context, holiday core.Holiday) []*core.Holiday {
+func (_m *HolidayService) GetHolidays(ctx context.Context, holiday core.Holiday) []core.Holiday {
 	ret := _m.Called(ctx, holiday)
 
-	var r0 []*core.Holiday
-	if rf, ok := ret.Get(0).(func(context.Context, core.Holiday) []*core.Holiday); ok {
+	var r0 []core.Holiday
+	if rf, ok := ret.Get(0).(func(context.Context, core.Holiday) []core.Holiday); ok {
 		r0 = rf(ctx, holiday)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]*core.Holiday)
+			r0 = ret.Get(0).([]core.Holiday)
 		}
 	}
 
